795b94451732e160023501f92a764aa4b32e512f
Mandar Chitre <mandar@arl.nus.edu.sg>
2018-07-11 02:02:49 +0800

    Add disk free information

--- scripts/modem.sh
+++ scripts/modem.sh
@@ -7,6 +7,9 @@ cat /tmp/modem.txt | python scripts/modem_temperature.py
 cat /tmp/modem.txt | python scripts/modem_noise.py
 rm -f /tmp/modem.txt
 
+DF=`df -m /mnt | tail -1 | perl -pe 's/ +/,/g' | cut -d',' -f3,4`
+echo "{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}" > data/df.json
+
 git add data/*
 git commit -m "Data updated"
 git push
--- scripts/node_c.sh
+++ scripts/node_c.sh
@@ -4,6 +4,9 @@ cd /home/odroid/netiquette-dashboard
 
 ls -1rt ../logs/chargerLog_NODE_C_* | tail -2 | sed 's/^/cat /' | sh | python scripts/node_c_pv.py
 
+DF=`df -m / | tail -1 | perl -pe 's/ +/,/g' | cut -d',' -f3,4`
+echo "{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}" > data/df.json
+
 git add data/*
 git commit -m "Data updated"
 git push
