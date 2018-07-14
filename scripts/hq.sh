11178a8a1c9afb919d02f373eade9748f8fc84c4
Mandar Chitre <mandar@arl.nus.edu.sg>
2018-07-14 18:46:25 +0800

    Separated disk free files for datastore and hq

--- scripts/hq.sh
+++ scripts/hq.sh
@@ -3,9 +3,9 @@
 cd /home/nethq/netiquette-dashboard
 
 DF=`df -m / | tail -1 | perl -pe 's/ +/,/g' | cut -d',' -f3,4`
-echo "[{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}" > data/df.json
+echo "{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}" > data/df.json
 DF=`df -m /mnt/datastore | tail -1 | perl -pe 's/ +/,/g' | cut -d',' -f3,4`
-echo "{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}]" >> data/df.json
+echo "{\"labels\": [\"Used\",\"Free\"], \"series\": [$DF]}" > data/dfDS.json
 
 find ../logs -type f -name aisLog* -mtime -1 -print | sort | sed 's/^/cat /' | sh | python scripts/hq_ais.py
 
